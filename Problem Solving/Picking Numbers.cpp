9d02962561f0763646d72bc411b78964a2517c76
jatin verma <42065404+jv640@users.noreply.github.com>
2020-03-26 09:50:58 +0530

    Update Picking Numbers.cpp

--- Problem Solving/Picking Numbers.cpp
+++ Problem Solving/Picking Numbers.cpp
@@ -1,109 +1,18 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'pickingNumbers' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY a as parameter.
- */
-
+int max(int a, int b){
+    if(a>b)return a;
+    else return b;
+}
 int pickingNumbers(vector<int> a) {
-    sort(a.begin(),a.end());
-    vector<int>:: iterator it1,it2;
-    it1=it2=a.begin();
-    int ans=0,count=0,compare=a[0];
-    bool flag=true;
-    while(it2!=a.end())
+    vector <int> arr(100, 0);
+    int n = a.size();
+    for(int i=0;i<n;i++)
     {
-        if(abs(*it2-compare)<2)
-        {
-            count++;
-        }
-        else{
-            it2=it1;
-            compare=*it2;
-            count=0;
-            flag=true;
-        }
-        if(count>ans)ans=count;
-        if(*it2!=compare && flag==true){it1=it2;flag=false;}
-        it2++;
+        arr[a[i]]++;
     }
-return ans;
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
-
-    vector<string> a_temp = split(rtrim(a_temp_temp));
-
-    vector<int> a(n);
-
-    for (int i = 0; i < n; i++) {
-        int a_item = stoi(a_temp[i]);
-
-        a[i] = a_item;
+    int result = 0;
+    for(int i=1;i<100;i++)
+    {
+        result = max(result, arr[i]+arr[i-1]);
     }
-
-    int result = pickingNumbers(a);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
+    return result;
 }
